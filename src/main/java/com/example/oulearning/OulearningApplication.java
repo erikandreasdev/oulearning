4a9d032f2c2e5520d140ceb19b65f5a21f5e6fc6
Erik Andreas Barreto de Vera <erik.dvera@gmail.com>
2026-08-17 17:50:53 +0100

    init

---
+++ src/main/java/com/example/oulearning/OulearningApplication.java
@@ -0,0 +1,13 @@
+package com.example.oulearning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OulearningApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OulearningApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/oulearning/OulearningApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.oulearning;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OulearningApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
